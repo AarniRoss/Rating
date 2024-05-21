f0b9ee2af1d669d63c63f4b7c213a39d1e078f15
AarniRoss <167082415+AarniRoss@users.noreply.github.com>
2024-05-21 11:47:00 +0300

    Juttuja

---
+++ JavaS/button.js
@@ -0,0 +1,15 @@
+
+
+
+const rateButtons = document.getElementsByClassName ('number-circle');
+
+console.log(rateButtons);
+
+function rateActive() {
+  newBackgroundColor.style.backgroundColor = 'orange';
+};
+
+newBackgroundColor.addEventListener("click", rateActive)
+
+
+console.log(rateButtons);
