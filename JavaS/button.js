6a90ca2919c86a6f0eef3cd015dbcdc53e0dfcde
AarniRoss <167082415+AarniRoss@users.noreply.github.com>
2024-05-30 14:34:33 +0300

    Nappi töitä

--- JavaS/button.js
+++ JavaS/button.js
@@ -1,15 +1,38 @@
 
 
+let rateButtons = document.getElementById ('number-circle');
+let subButton = document.getElementById ('submit-button');
 
-const rateButtons = document.getElementsByClassName ('number-circle');
+let submittedBoxStatus = document.getElementById ('submitted-box');
+let questionBoxStatus = document.getElementById ('question-box');
 
-console.log(rateButtons);
+//
+function buttonColorChange() {
+  rateButtons.style.background  = "orange";
+}
 
-function rateActive() {
-  newBackgroundColor.style.backgroundColor = 'orange';
+/*
+subButton.addEventListener('click', function() {
+  if (questionBoxStatus.style.display !== 'none') {
+    questionBoxStatus.style.display = 'none';
+    submittedBoxStatus.style.display = 'block';
+  } else {
+    questionBoxStatus.style.display = 'block';
+    submittedBoxStatus.style.display = 'none';
+  }
+});
+*/
+
+
+
+
+function proceed() {
+if () === null
+}
+
+
+
+function submit() {
+  submittedBoxStatus.classList.remove('hidden-box');
+  questionBoxStatus.classList.add('hidden-box');
 };
-
-newBackgroundColor.addEventListener("click", rateActive)
-
-
-console.log(rateButtons);
